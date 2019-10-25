12f6a50c035b21eac981eb40fc4d6c00b61be55f
Phạm Quốc Sang <56308260+PhamQuocSang@users.noreply.github.com>
2019-10-25 13:30:42 +0700

    App ver 1.1

--- src/main/java/Buoi12_BTNganHangVaKhachHang2/App.java
+++ src/main/java/Buoi12_BTNganHangVaKhachHang2/App.java
@@ -1,5 +1,6 @@
 package Buoi12_BTNganHangVaKhachHang2;
 
+import Bank.Bank;
 import Bank.NHHSBC;
 import Bank.NHShinHan;
 import KhachHang.KhachHang;
@@ -15,6 +16,8 @@ import java.util.Scanner;
 public class App {
     static Scanner scanner = new Scanner(System.in);
     static ArrayList<KhachHang> listKhachHang = new ArrayList<KhachHang>();
+    static ArrayList<NHHSBC> listBank = new ArrayList<NHHSBC>();
+    static ArrayList<NHShinHan> listBank2 = new ArrayList<NHShinHan>();
 
     public static void main(String[] args) {
         KhachHangController cc = new KhachHangController();
@@ -22,7 +25,8 @@ public class App {
             NHShinHan nhShinHan = new NHShinHan();
             NHHSBC nhhsbc = new NHHSBC();
             System.out.println("_________________________________________");
-            System.out.println("Chọn một trong các phương thức hành động sau: \n1. Thêm khách hàng HSBC \n2. Hiển thị khách hàng HSBC \n3. Thực hiện giao dịch");
+            System.out.println("Chọn một trong các phương thức hành động sau: \n1. Thêm khách hàng HSBC \n2. Hiển thị khách hàng HSBC \n3. Thực hiện giao dịch HSBC");
+            System.out.println("\n4. Thêm khách hàng ShinHan \n5. Hiển thị khách hàng ShinHan \n6. Thực hiện giao dịch ShinHan \n\n7. Hiển thị danh sách khách hàng của cả 2 ngân hàng");
             System.out.println("_________________________________________");
             System.out.print("Chọn: ");
             String s = scanner.nextLine();
@@ -40,7 +44,19 @@ public class App {
 //                    nhhsbc.thucHienChuyenTien();
                     nhhsbc.thucHienChuyenTien2();
                     break;
-
+                case "4":
+                    nhShinHan.themKhachHangSHINHAN();
+                    break;
+                case "5":
+                    nhShinHan.hienThiKhachHangSHINHAN();
+                    break;
+                case "6":
+                    nhShinHan.thucHienChuyenTien2();
+                    break;
+                case "7":
+                    nhhsbc.hienThiKhachHangHSBC();
+                    nhShinHan.hienThiKhachHangSHINHAN();
+                    break;
                 default:
                     break;
             }
